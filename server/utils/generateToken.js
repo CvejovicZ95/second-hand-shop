2c5ab201b06149808cf12c271c86a97c442debb0
Zoran Cvejovic <cvejovicz@gmail.com>
2024-04-10 19:00:26 +0200

    test again

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (user, res) => {
   res.cookie('token',token,{
     maxAge:24*60*60*1000,
     httpOnly:true,
-    sameSite:'none',
+    sameSite:'strict',
   });
   //return token
 };
